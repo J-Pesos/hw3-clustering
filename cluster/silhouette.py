cf2b8e58be0cf7d8c4c69bd25edf01e69408e6c4
Joaquin Magana <joaquinmmagana@gmail.com>
2023-02-14 18:54:56 -0800

    Updating silhouette

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -24,7 +24,7 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
-        print("Silhouette v6")
+        print("Silhouette v9.2.2")
         # Basic error handling.
         assert X.size > 0, 'You must provide a non-empty X matrix.'
         assert Y.size > 0, 'You must provide a non-empty Y matrix.'
@@ -32,7 +32,6 @@ class Silhouette:
 
         # Initialize list of scores.
         scores = np.zeros(X.shape[0])
-        #print(scores)
 
         # Calculate score for each observation in the matrix.
         for i,row in enumerate(X):
@@ -42,27 +41,24 @@ class Silhouette:
             for label in np.unique(Y):
 
                 if label == row_label:
-                    points = X[np.where(np.delete(Y,i) == label)]
-                    #print(points)
-                    #print(np.size(points))
-                    if np.size(points) == 0: # If there are no other points in label, calculate mean distance 0.
+                    indices = Y == label
+                    indices[i] = False
+                    print(indices)
+                    if any(indices) == False: # If there are no other index values matching label, calculate mean distance 0.
                         intra_dist_mean = 0
+                        break
                     else:
-                        intra_dist_mean = np.mean(cdist([row], points))
-                    #print(intra_dist_mean)
+                        intra_dist_mean = np.mean(cdist([row], X[indices]))
                 else:
-                    points = X[np.where(Y == label)]
+                    points = X[np.where(Y == label)] # If there are no other points in label, calculate mean distance 0.
                     if np.size(points) == 0:
                         inter_dist_mean += 0
                     else:
                         inter_dist_mean += [np.mean(cdist([row], points))]
-                    #print(inter_dist_mean)
                     
-
             min_inter_dist_mean = min(inter_dist_mean)
             score = (min_inter_dist_mean - intra_dist_mean) / max(intra_dist_mean, min_inter_dist_mean)
 
             scores[i] = score
-            #print(scores)
         
         return scores
